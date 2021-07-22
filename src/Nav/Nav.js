d9eda1ff24b940398a32fcb2875149d5f48cdb59
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-07-21 22:12:27 -0400

    Updated Nav bar to exclude a href and utilized Links only instead

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,26 +4,18 @@ import { Link } from "react-scroll";
 const Nav = () => {
   return (
     <div className="topNavBar">
-      <a href="#home" className="activeLink">
-        <Link to="header-container" smooth={true} duration={1200}>
-          Home
-        </Link>
-      </a>
-      <a href="#about">
-        <Link to="about-container" smooth={true} duration={1200}>
-          About
-        </Link>
-      </a>
-      <a href="https://www.amazon.com/s?me=A366LPOP2A82ZM&marketplaceID=ATVPDKIKX0DER">
-        <Link to="product-container" smooth={true} duration={1200}>
-          Products
-        </Link>
-      </a>
-      <a href="#contact">
-        <Link to="contact-container" smooth={true} duration={1200}>
-          Contact
-        </Link>
-      </a>
+      <Link to="header-container" smooth={true} duration={1200}>
+        Home
+      </Link>
+      <Link to="about-container" smooth={true} duration={1200}>
+        About
+      </Link>
+      <Link to="product-container" smooth={true} duration={1200}>
+        Products
+      </Link>
+      <Link to="contact-container" smooth={true} duration={1200}>
+        Contact
+      </Link>
     </div>
   );
 };
