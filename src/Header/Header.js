49c9a1dec6fe6c2dfd2bc3b4aea1b915025eac1f
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-08-09 21:09:54 -0400

    Updated Navbar and header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,4 @@
 import "./Header.css";
-import { Link } from "react-scroll";
-import jouleLogo from "../images/JOULE_LOGO_HEADER.png";
 import jouleVideo from "../images/jouleVideo.mp4";
 
 const Header = () => {
@@ -11,16 +9,8 @@ const Header = () => {
         <video autoPlay muted loop id="jouleVideo">
           <source src={jouleVideo} type="video/mp4" />
         </video>
-        <div className="logo-container">
-          <img src={jouleLogo} alt="Joule Logo" className="logo" />
-        </div>
         <div className="button-container">
-          <h3>GET WORK DONE!</h3>
-          <button className="btn-primary">
-            <Link to="product-container" smooth={true} duration={1200}>
-              Shop Knee Sleeves
-            </Link>
-          </button>
+          <h3>Get work done.</h3>
         </div>
       </div>
     </div>
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,22 +1,86 @@
-import "./Nav.css";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import "./Nav.css";
+import jouleLogo from "../images/JOULE_LOGO_HEADER.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTimes,
+  faBars,
+  faEnvelope,
+  faUserFriends,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    setSelected(!selected);
+  };
+
   return (
-    <div className="topNavBar">
-      <Link to="header-container" smooth={true} duration={1200}>
-        Home
-      </Link>
-      <Link to="about-container" smooth={true} duration={1200}>
-        About
-      </Link>
-      <Link to="product-container" smooth={true} duration={1200}>
-        Products
-      </Link>
-      <Link to="contact-container" smooth={true} duration={1200}>
-        Contact
-      </Link>
-    </div>
+    <nav className="topNavBar">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <img src={jouleLogo} alt="Joule Logo" className="logo" />
+        </div>
+        <div className="media-icon-container">
+          <FontAwesomeIcon icon={faUserFriends} className="fb-icon" />
+          <FontAwesomeIcon icon={faEnvelope} className="email-icon" />
+        </div>
+        <ul className={selected ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link
+              to="header-container"
+              className="nav-link"
+              smooth={true}
+              duration={1200}
+              onClick={handleSelect}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="about-container"
+              className="nav-link"
+              smooth={true}
+              duration={1200}
+              onClick={handleSelect}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="product-container"
+              className="nav-link"
+              smooth={true}
+              duration={1200}
+              onClick={handleSelect}
+            >
+              Products
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="contact-container"
+              className="nav-link"
+              smooth={true}
+              duration={1200}
+              onClick={handleSelect}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className="nav-icon" onClick={handleSelect}>
+          <FontAwesomeIcon
+            icon={selected ? faTimes : faBars}
+            className={selected ? "fas fa-times" : "fas fa-bars"}
+          />
+        </div>
+      </div>
+    </nav>
   );
 };
 
